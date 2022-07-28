daa5a6c87732cbc46d57f8f8d93724b95efbf947
projectperfect <jprfct@protonmail.com>
2022-07-28 01:14:25 -0500

    hard level completed.

--- main.py
+++ main.py
@@ -11,15 +11,39 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level 
 
-password = ""
+#password = ""
+
+#for char in range(1, nr_letters + 1):
+    #password += random.choice(letters)
+
+#for char in range(1, nr_symbols + 1):
+    #password += random.choice(symbols)
+
+#for char in range(1, nr_numbers + 1):
+    #password += random.choice(numbers)
+
+#print(password)
+
+#Hard level
+
+password_list = []
 
 for char in range(1, nr_letters + 1):
-    password += random.choice(letters)
+    password_list.append(random.choice(letters))
 
 for char in range(1, nr_symbols + 1):
-    password += random.choice(symbols)
+    password_list += random.choice(symbols)
 
 for char in range(1, nr_numbers + 1):
-    password += random.choice(numbers)
+    password_list += random.choice(numbers)
 
-print(password)
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
